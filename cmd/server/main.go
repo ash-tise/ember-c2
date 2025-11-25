6322f2c9c63f21955448b22e772c3113c0e9f62b
ash <ash.tise24@gmail.com>
2025-11-25 08:08:47 -0600

    Build basic logger and handleResister func

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/ash-tise/ember-c2/internal/proto"
+)
+
+func initLogger() {
+
+	// create or append to logfile
+	logFile, err := os.OpenFile("ember.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("FATAL: Could not open log file: %v", err)
+	}
+	// initialize multiwriter for logging to stdout and file
+	multiwriter := io.MultiWriter(os.Stdout, logFile)
+
+	// configure logging options
+	log.SetOutput(multiwriter)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	log.Println("Ember Logger Initialized. Writing to ember.log and Console.")
+}
+
+func handleRegister(w http.ResponseWriter, r *http.Request) {
+
+	var initBeacon proto.Beacon
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&initBeacon); err != nil {
+		log.Printf("ERROR: handleRegister: Failed to decode initBeacon JSON: %v", err)
+		http.Error(w, "Invalid request format", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	log.Println("OPERATIONAL: Received registration attempt from Host:", initBeacon.Hostname)
+}
+
 func main() {
-	// start of server
+	initLogger()
 }
