c4309c04063c0d4a128b6b248e9e6f2728433e14
ash <ash.tise24@gmail.com>
2025-11-25 07:31:34 -0600

    Finished prototocol structs

--- internal/proto/proto.go
+++ internal/proto/proto.go
@@ -1,3 +1,28 @@
 package proto
 
-// define messages in here
+import "time"
+
+type Beacon struct {
+	AgentID  string `json:"id"`
+	Hostname string `json:"host"`
+	OS       string `json:"os"`
+	Result   string `json:"r"`
+}
+
+type Command struct {
+	TaskID    string `json:"tid"`
+	Action    string `json:"act"`
+	Arguments string `json:"args"`
+}
+
+type Response struct {
+	Commands []Command `json:"cmds"`
+}
+
+type AgentMetadata struct {
+	AgentID      string
+	Hostname     string
+	OS           string
+	LastCheckIn  time.Time
+	CommandQueue chan Command
+}
